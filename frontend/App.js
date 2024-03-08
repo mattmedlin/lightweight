df4e949d31b5fd4e86108e554f41e471997494c5
Matt Medlin <mattmedlin@Matts-MBP.attlocal.net>
2024-03-07 22:57:09 -0600

    frontend with tailwind

---
+++ frontend/App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-900 ">
+      <Text className="text-white">
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
