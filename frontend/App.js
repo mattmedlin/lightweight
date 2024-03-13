2963fd9b8566200ac7554ab38c25b912d9bf29b2
Matt Medlin <mattmedlin@Matts-MBP.attlocal.net>
2024-03-12 22:48:40 -0500

    basic auth with clerk

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import Constants from "expo-constants";
+import SignInWithOAuth from "./components/SignInWithOAuth";
+import SignInScreen from "./components/SignInScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider
+      publishableKey={Constants.expoConfig.extra.clerkPublishableKey}
+    >
+      <SafeAreaView styles={styles.container}>
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <SignInScreen />
+          <SignInWithOAuth />
+        </SignedOut>
+      </SafeAreaView>
+    </ClerkProvider>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
