58bbbc82d5f035fe946b70f963fa26a0f491a54e
Matt Medlin <mattmedlin@Matts-MBP.attlocal.net>
2024-03-30 18:24:51 -0500

    create urls

--- backend/lightweight/lightweight/urls.py
+++ backend/lightweight/lightweight/urls.py
@@ -16,7 +16,38 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ..api.views import (
+    UserListCreateAPIView,
+    UserDetailAPIView,
+    WorkoutListCreateAPIView,
+    WorkoutDetailAPIView,
+    EquipmentListCreateAPIView,
+    EquipmentDetailAPIView,
+    ExerciseListCreateAPIView,
+    ExerciseDetailAPIView,
+    ExerciseDetailListCreateAPIView,
+    ExerciseDetailDetailAPIView,
+    MuscleGroupListCreateAPIView,
+    MuscleGroupDetailAPIView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', UserListCreateAPIView.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetailAPIView.as_view(), name='user-detail'),
+
+    path('workouts/', WorkoutListCreateAPIView.as_view(), name='workout-list'),
+    path('workouts/<int:pk>/', WorkoutDetailAPIView.as_view(), name='workout-detail'),
+
+    path('equipment/', EquipmentListCreateAPIView.as_view(), name='equipment-list'),
+    path('equipment/<int:pk>/', EquipmentDetailAPIView.as_view(), name='equipment-detail'),
+
+    path('exercises/', ExerciseListCreateAPIView.as_view(), name='exercise-list'),
+    path('exercises/<int:pk>/', ExerciseDetailAPIView.as_view(), name='exercise-detail'),
+
+    path('exercise-details/', ExerciseDetailListCreateAPIView.as_view(), name='exercise-detail-list'),
+    path('exercise-details/<int:pk>/', ExerciseDetailDetailAPIView.as_view(), name='exercise-detail-detail'),
+
+    path('muscle-groups/', MuscleGroupListCreateAPIView.as_view(), name='muscle-group-list'),
+    path('muscle-groups/<int:pk>/', MuscleGroupDetailAPIView.as_view(), name='muscle-group-detail'),
 ]
