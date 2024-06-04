3dca346744e7cb0ba4913da5f89e2dc7e6b2383b
Matt Medlin <mattmedlin@Matts-MacBook-Pro.local>
2024-06-03 21:32:32 -0400

    remove unused imports

--- api/src/queries.ts
+++ api/src/queries.ts
@@ -1,4 +1,4 @@
-import { asc, between, count, eq, getTableColumns, sql } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import { db } from "./db";
 import { InsertUser, SelectUser, users } from "./schema";
 
